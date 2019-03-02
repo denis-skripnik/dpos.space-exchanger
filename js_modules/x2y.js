01e39817a0fe5f47ad487eaaa28c526f4e58c652
denis-skripnik <scadens@yandex.ru>
2019-03-03 03:04:36 +0700

    Changed and renamed
    Изменил кое что в файле x2y.js, например, добавил проверку gbg/sbd с возвратом средств, если был перевод в данном токене. Также был переименован тестовый файл для проведения 500 переводов в одной транзакции.

--- js_modules/x2y.js
+++ js_modules/x2y.js
@@ -10,101 +10,93 @@ for (let blockchain in conf) {
 }
 }
 
+async function sendTransfer(chain, active_key, from, to, amount, memo) {
+    const transfer = await methods.transfer(chain, active_key, from, to, amount, memo);
+    if (transfer !== 0) {
+        const time = await helpers.unixTime();
+        await trxdb.addTransaction(transfer, from, to, amount, memo, time, chain);
+        return 1;
+        } else {
+        return 0;
+        }
+}
+
 async function processTransfer(x, data) {
 var x_token = conf[x].token;
     var memo = data.memo;
 memo = memo.toLowerCase();
     var exchange_data = memo.split(":"); // 0 - блокчейн, 1 - логин.
     if (chains.includes(exchange_data[0])) {
-    var y = exchange_data[0];
-    } else {
-var y = x;
-    }
-    var y_token = conf[y].token;
-var to = exchange_data[1];
-var curs = conf[x].how_to_viz/conf[y].how_to_viz;
+        let y = exchange_data[0];
+        let y_token = conf[y].token;
+let to = exchange_data[1];
+let curs = conf[x].how_to_viz/conf[y].how_to_viz;
 curs *= 1000;
 curs = parseInt(curs);
 curs /= 1000;
-var fee = conf[x].fee;
-var amount = parseFloat(data.amount)*curs;
-var amount_fee = 0.01*amount*fee;
+let fee = conf[x].fee;
+let amount = parseFloat(data.amount)*curs;
+let amount_fee = 0.01*amount*fee;
 amount = amount - amount_fee;
 amount *= 1000;
 amount = parseInt(amount);
 amount /= 1000;
 amount = amount.toFixed(3);
-    try {
+let receive_approve;
+try {
         const exchange_receiver = await methods.getAccount(y, to);
 if (exchange_receiver.length > 0) {
-    var receive_approve = true;
+    receive_approve = 'ok';
 } else {
-    var receive_approve = false;
+  receive_approve = 'noAccount';
 }
 } catch(e) {
-    var receive_approve = 'noConnection';
+    receive_approve = 'noConnection';
 console.log(e);
 }
 
 try {
 const from_account = await methods.getAccount(y, conf[y].login);
-var balance = parseFloat(from_account[0].balance);
+let balance = parseFloat(from_account[0].balance);
 if (balance < amount) {
     receive_approve = 'noBalance';
 }
 } catch(e) {
-var receive_approve = 'noConnection';
+receive_approve = 'noConnection';
 console.log(e);
 }
+
+if (x === 'golos' || x === 'steem') {
+    let sender_amount = data.amount.split(' ');
+    let sender_token = sender_amount[1];
+if (sender_token === 'GBG' || sender_token === 'SBD') {
+receive_approve = 'noSupportedToken';
+}
+}
+
 console.log(`Блокчейн-получатель: ${y}, статус: ${receive_approve}.`);
-try {
-    if (receive_approve === true) {
+if (receive_approve === 'ok') {
         amount = amount + ` ${y_token}`;
-       
-        var memo = `Обмен ${x_token} на ${y_token}. Шлюз обмена: @${conf[y].login}, 1 ${x_token} =${curs} ${y_token}.`;
-        const transfer = await methods.transfer(y, conf[y].active_key, conf[y].login, to, amount, memo);
-        if (transfer !== 0) {
-            const time = await helpers.unixTime();
-await trxdb.addTransaction(transfer, conf[y].login, to, amount, memo, time, y);
-return 1;
-} else {
-return 0;
-}
+      let memo = `Обмен ${x_token} на ${y_token}. Шлюз обмена: @${conf[y].login}, 1 ${x_token} =${curs} ${y_token}.`;
+return await sendTransfer(y, conf[y].active_key, conf[y].login, to, amount, memo);
 } else     if (receive_approve === 'noBalance') {
-    var memo = `Баланс у шлюза @${conf[x].login} меньше, чем необходимо вам отдать. Просьба смотреть максимальную сумму в описании аккаунта.`;
-    const transfer = await methods.transfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
-    if (transfer !== 0) {
-        const time = await helpers.unixTime();
-        await trxdb.addTransaction(transfer, conf[x].login, data.from, data.amount, memo, time, x);
-        return 1;
-    } else {
-        return 0;
-        }
-        } else     if (receive_approve === 'noConnection' || receive_approve === undefined) {
-    var memo = `К сожалению, не удалось соединиться с Нодой для обмена. Возможно вы указали неподдерживаемый блокчейн: поддерживается golos, steem, whaleshares, viz. Повторите попытку, пожалуйста.`;
-    const transfer = await methods.transfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
-    if (transfer !== 0) {
-        const time = await helpers.unixTime();
-        await trxdb.addTransaction(transfer, conf[x].login, data.from, data.amount, memo, time, x);
-        return 1;
-        } else {
-        return 0;
-        }
-} else if (receive_approve === false) {
-var memo = `Аккаунт ${to} не существует в блокчейне ${y}.`;
-const transfer = await methods.transfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
-if (transfer !== 0) {
-    const time = await helpers.unixTime();
-    await trxdb.addTransaction(transfer, conf[x].login, data.from, data.amount, memo, time, x);
-    return 1;
-} else {
-    return 0;
+    let memo = `Вы отправили сумму, превышающую максимальную. Просьба смотреть максимумы в описании аккаунта шлюза @${conf[x].login}.`;
+    return await sendTransfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
+} else     if (receive_approve === 'noSupportedToken') {
+    let memo = `Токен не поддерживвается. Пожалуйста, отправьте сумму в ${x_token}`;
+    return await sendTransfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
+} else if (receive_approve === 'noConnection' || receive_approve === undefined) {
+        let memo = `К сожалению, не удалось соединиться с Нодой для обмена. Повторите попытку, пожалуйста.`;
+        return await sendTransfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
+    } else if (receive_approve === 'noAccount') {
+    let memo = `Аккаунт ${to} не существует в блокчейне ${y}.`;
+    return await sendTransfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
     }
-}
-} catch(e) {
-    console.log(e);
-    }
-   
+    } else if (!chains.includes(exchange_data[0])) {
+        console.log('Блокчейн: не существующий');
+        let memo = `Вероятно вы указали неподдерживаемый блокчейн или ошиблись в написании: поддерживается golos, steem, whaleshares, viz. Повторите попытку, пожалуйста.`;
+        return await sendTransfer(x, conf[x].active_key, conf[x].login, data.from, data.amount, memo);
+       }
 }
 
 async function processBlock(x, bn) {
@@ -114,7 +106,7 @@ let ok_ops_count = 0;
         const [op, opbody] = tr.op;
         switch(op) {
                 case "transfer":
-if (opbody.to === conf[x].login && opbody.memo.indexOf("Шлюз обмена:") === -1 && opbody.memo.indexOf("с Нодой для обмена") === -1 && opbody.memo.indexOf("Баланс у шлюза @") === -1 && opbody.memo.indexOf("withdraw:") === -1 && opbody.memo.indexOf("return:") === -1) {
+if (opbody.to === conf[x].login && opbody.memo.indexOf("Шлюз обмена:") === -1 && opbody.memo.indexOf("с Нодой для обмена") === -1 && opbody.memo.indexOf("Вы отправили сумму, превышающую максимальную") === -1 && opbody.memo.indexOf("withdraw:") === -1 && opbody.memo.indexOf("return:") === -1 && opbody.memo.indexOf("не существует в блокчейне ") === -1 && opbody.memo.indexOf("Вероятно вы указали неподдерживаемый блокчейн или ошиблись в написании:") === -1 && opbody.memo.indexOf("Токен не поддерживвается.") === -1) {
     ok_ops_count += await processTransfer(x, opbody);
 }
                 break;
